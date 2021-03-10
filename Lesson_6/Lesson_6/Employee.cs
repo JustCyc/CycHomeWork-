7f9feb2351e52a4f3b2a85bd853959934690d3e9
Cyc <chernous.alex@gmail.com>
2021-03-10 18:56:22 +0300

    HM6

---
+++ Lesson_6/Lesson_6/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_6
+{
+    public class Employee
+    {
+        
+        
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public string TelNumber { get; set; }
+        public int Salary { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string name, string pos, string email, string tel, int sal, int age)
+        {
+            Name = name;
+            Position = pos;
+            Email = email;
+            TelNumber = tel;
+            Salary = sal;
+            Age = age;
+        }
+        //public static string SelectEmployee(Employee[] empArray)
+        //{
+        //    for (int i = 0; i < empArray.Length; i++)
+        //    {
+        //        if (empArray[i].Age > 40)
+        //        {
+                    
+        //        }
+        //    }
+            
+        //}
+    }
+
+}
+
+
---
+++ Lesson_6/Lesson_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание с массивом 
+            string[,] array = new string[4, 4];
+            array[0, 0] = array[1, 2] = array[3, 0] = array[3, 2] = "1";
+            array[0, 1] = array[1, 3] = array[2, 0] = array[2, 1] = "2";
+            array[0, 3] = array[1, 0] = array[2, 2] = array[3, 3] = "3";
+            array[0, 2] = array[2, 3] = array[3, 1] = "4";
+            array[1, 1] = "find me";
+            try
+            {
+                Console.WriteLine(ArrayException.ConvertAndSum(array));
+            }
+            catch(ArrayException.MyArraySizeException)
+            {
+                Console.WriteLine(" Неверный входной размер массива\n");
+            }
+            catch(ArrayException.MyArrayDataException)
+            {
+                Console.WriteLine(" Неверный тип ячейки массива\n");
+            }
+
+
+            //Employee[] empArray = new Employee[5];
+            //for (int i = 0; i < empArray.Length; i++)
+            //{
+            //    empArray[i] = new Employee("Иванов", "кто", "e", "123", 1200, 30);
+            //    empArray[i] = new Employee("Иванов", "кто", "e", "123", 1200, 20);
+            //    empArray[i] = new Employee("Иванов", "кто", "e", "123", 1200, 40);
+            //    empArray[i] = new Employee("Иванов", "кто", "e", "123", 1200, 45);
+            //    empArray[i] = new Employee("Иванов", "кто", "e", "123", 1200, 32);
+            //}
+            //Console.WriteLine(Employee.SelectEmployee(empArray));
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
