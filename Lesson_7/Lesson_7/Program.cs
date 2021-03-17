1b210854090d3c8ce5c27dd19172ce85c2f650e4
Cyc <chernous.alex@gmail.com>
2021-03-17 18:39:35 +0300

    Lesson_7 incomplete

---
+++ Lesson_7/Lesson_7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, квадрат которого хотите получить");
+            Console.WriteLine($"Результат: {Square.square(int.Parse(Console.ReadLine()))}");
+            Console.ReadKey();
+
+            //    Cross.InitField();
+            //    Cross.PrintField();
+
+            //    while (true)
+            //    {
+            //        Cross.PlayerStep();
+            //        Cross.PrintField();
+            //        if (Cross.CheckWin(Cross.PLAYER_DOT))
+            //        {
+            //            Console.WriteLine("Player Win!");
+            //            break;
+            //        }
+            //        if (Cross.IsFieldFull())
+            //        {
+            //            Console.WriteLine("DRAW");
+            //            break;
+            //        }
+            //        Cross.AiStep();
+            //        Cross.PrintField();
+            //        if (Cross.CheckWin(Cross.AI_DOT))
+            //        {
+            //            Console.WriteLine("SkyNet Win!");
+            //            break;
+            //        }
+            //        if (Cross.IsFieldFull())
+            //        {
+            //            Console.WriteLine("DRAW");
+            //            break;
+            //        }
+            //    }
+            //    Console.ReadKey();
+        }
+    }
+
+}
---
+++ Lesson_7/Lesson_7/Square.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_7
+{
+    class Square
+    {
+        static public int square(int a)
+        {
+            int square = a * a;
+            return square;
+        }
+    }
+}
