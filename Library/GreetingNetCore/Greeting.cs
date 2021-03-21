6e34920058fe346d08cf5dbfa64be6b33554dd61
Cyc <chernous.alex@gmail.com>
2021-03-21 18:45:32 +0300

    HM Lesson_8 incomplete

---
+++ Library/GreetingNetCore/Greeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreetingNetCore
+{
+    public class Greeting
+    {
+        public static void HelloMate()
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine($" Hello mate, the time is now {date}");
+        }
+    }
+}
